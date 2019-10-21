1e22a5059aca351b110334c2595c9d1247a0da58
hache <chmartinez35@gmail.com>
2019-10-21 23:37:16 +0200

    feat(Icon): Add support for style prop & set default styles

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import { size } from 'style';
+
 import InternalIcon from './icons-supported';
 
-const Icon = (props) => (<i><InternalIcon {...props} /></i>);
+const StyledIcon = styled.i`
+    display: inline-block;
+    height: ${size.icon.normal};
+    width: ${size.icon.normal};
+    font-size: ${size.icon.normal};
+`;
+
+// TODO: add test and refactor this s***!
+const Icon = ({style, ...props}) => (<StyledIcon style={style}><InternalIcon width={'1em'} height={'1em'} {...props} /></StyledIcon>);
 
 export default Icon;
--- src/style/size.js
+++ src/style/size.js
@@ -3,6 +3,7 @@ import unit from './unit';
 const createMolecule = (value) => ({
     small: value.val(0.75),
     medium: value.val(1.25),
+    normal: value.val(1),
     large: value.val(1.5),
 });
 
@@ -14,3 +15,5 @@ export const border = atom.val(0.0625); // forces border to be 1px (in rems)
 export const radius = atom.val(0.3750); // radius = 6px (in rems)
 export const margin = createMolecule(atom);
 export const padding = createMolecule(atom);
+// TODO: re-define the way we create sizes!
+export const icon = createMolecule(unit(2));
